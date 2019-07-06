ce1016397ce29a9961e66976e18e6badab74c220
Deepak Saraf <389saraf@gmail.com>
2019-07-06 12:47:59 +0000

    modified Subtract

--- calc.py
+++ calc.py
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 def add(x+y):
 return x+y
 def subtract(x,y):
 pass
-=======
 def(x,y):
 pass
-def subtract(x-y):
-return x-y
->>>>>>> Sub
 def multiply(x,y):
 return x*y
 def divide(x,y):
